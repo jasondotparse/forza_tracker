108f7a1593df23390e2e0be05356025edf256faa
Holtkamp, Jason <jholtkamp@conversantmedia.com>
2018-10-10 22:59:32 -0500

    added vue boilerplate for frontend

--- server.js
+++ server.js
@@ -14,21 +14,15 @@ server.on('listening', function () {
 
 server.on('message', function (message, remote) {
   const raceIsOn = message.slice(0, 4).readInt8(0);
-  let timestamp; // possibly could be readInt32LE(0);
+  if (raceIsOn) {
 
-  const rpmMax = message.slice(8, 12).readFloatLE(0);
-  const rpmIdle = message.slice(12, 16).readFloatLE(0);
-  const rpmCurrent = message.slice(16, 20).readFloatLE(0);
+    const rpmMax = message.slice(8, 12).readFloatLE(0);
+    const rpmIdle = message.slice(12, 16).readFloatLE(0);
+    const rpmCurrent = message.slice(16, 20).readFloatLE(0);
 
-  const accelerationX = message.slice(20, 24).read(0);
-  const accelerationY = message.slice(24, 28).readDoubleLE(0);
-  const accelerationZ = message.slice(28, 32).readDoubleLE(0);
+    console.log(rpmCurrent, rpmIdle, rpmMax);
 
-  const velocityX = message.slice(32, 36).readFloatLE(0);
-  const velocityY = message.slice(36, 40).readFloatLE(0);
-  const velocityZ = message.slice(40, 44).readFloatLE(0);
-
-  console.log(rpmCurrent);
+  }
 });
 
 server.bind(PORT, HOST);
