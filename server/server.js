95ea77648ce3e5f8ee3ed673866a3b6557e83df9
Holtkamp, Jason <jholtkamp@conversantmedia.com>
2018-10-23 22:43:49 -0500

    refactoring

--- server/server.js
+++ server/server.js
@@ -1,47 +1,43 @@
 const PORT = 4200;
-const HOST = "10.0.0.118";
-const dgram = require("dgram");
-const server = dgram.createSocket("udp4");
-const express = require("express");
-const path = require("path");
+const HOST = '10.0.0.118';
+const dgram = require('dgram');
+const server = dgram.createSocket('udp4');
+const express = require('express');
+const path = require('path');
 const app = express();
 const { buildDataObj } = require('./utils');
 
-app.use(express.static(path.join(__dirname, "js")));
+app.use(express.static(path.join(__dirname, 'js')));
 
-let data = {};
+let data;
 
 app.listen(3000, () => {
   console.log(`listening on port 3000`);
 });
 
 app.get(`/`, (req, res) => {
-  res.sendFile(path.join(__dirname + "/index.html"));
+  res.sendFile(path.join(__dirname.slice(0, -6) + '/index.html'));
 });
 
 app.get(`/dist/build.js`, (req, res) => {
-  res.sendFile(path.join(__dirname + "/dist/build.js"));
+  res.sendFile(path.join(__dirname.slice(0, -6) + '/dist/build.js'));
 });
 
 app.get(`/data`, (req, res) => {
-  res.setHeader("Content-Type", "application/json");
+  res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify(data));
 });
 
-server.on("listening", function() {
+server.on('listening', function() {
   let address = server.address();
-  console.log(
-    "UDP Server listening on " + address.address + ":" + address.port
-  );
+  console.log(`UDP server is listening on ${address.address}:${address.port}`);
 });
 
 // make sure data out is on, IP address matches that above, and data output type is 'dash'.
-server.on("message", function(message, remote) {
+server.on('message', (message, remote) => {
   const raceIsOn = message.slice(0, 4).readUInt16LE(0); // s32
   if (raceIsOn) {
-
     data = buildDataObj(message);
-
     console.log('packet recieved');
   }
 });
--- server/utils.js
+++ server/utils.js
@@ -1,24 +1,23 @@
 const buildDataObj = packet => {
+  return {
+    // telemetry data
+    rpmMax: packet.slice(8, 12).readFloatLE(0), // f32
+    rpmIdle: packet.slice(12, 16).readFloatLE(0), // f32
+    rpmCurrent: packet.slice(16, 20).readFloatLE(0), // f32
+    speedMPH: packet.slice(244, 248).readFloatLE(0) * 2.23, // f32
 
-  const data = {};
-
-  data.rpmMax = packet.slice(8, 12).readFloatLE(0); // f32
-  data.rpmIdle = packet.slice(12, 16).readFloatLE(0); // f32
-  data.rpmCurrent = packet.slice(16, 20).readFloatLE(0); // f32
-
-  data.carOrdinal = packet.slice(212, 216).readUInt16LE(0); // s32
-  data.carClass = packet.slice(216, 220).readUInt16LE(0); // s32
-  data.carPerformanceIndex = packet.slice(220, 224).readUInt16LE(0); // s32
-  data.drivetrainType = packet.slice(224, 228).readUInt16LE(0); // s32
-  data.numCylinders = packet.slice(228, 232).readUInt16LE(0); // s32
-
-  data.speedMPH = packet.slice(244, 248).readFloatLE(0) * 2.23; // f32
-
-  data.raceBestLap = packet.slice(284, 288).readFloatLE(0); // f32
-  data.lastLap = packet.slice(288,292).readFloatLE(0) // f32
-  data.raceCurrentLap = packet.slice(292, 296).readFloatLE(0); // f32
-
-  return data;
+    // car stats
+    carOrdinal: packet.slice(212, 216).readUInt16LE(0), // s32
+    carClass: packet.slice(216, 220).readUInt16LE(0),// s32
+    carPerformanceIndex: packet.slice(220, 224).readUInt16LE(0), // s32
+    drivetrainType: packet.slice(224, 228).readUInt16LE(0), // s32
+    numCylinders: packet.slice(228, 232).readUInt16LE(0), // s32
+    
+    // race stats
+    raceBestLap: packet.slice(284, 288).readFloatLE(0), // f32
+    lastLap: packet.slice(288,292).readFloatLE(0), // f32
+    raceCurrentLap: packet.slice(292, 296).readFloatLE(0) // f32
+  };
 }
 
 module.exports = {
